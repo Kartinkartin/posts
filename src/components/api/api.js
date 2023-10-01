efc2b296d7b1ff0d0c1732d9b28fbb37e19cc414
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2023-10-01 16:24:07 +0300

    feat: add Rest methods & images

---
+++ src/components/api/api.js
@@ -0,0 +1,33 @@
+const config = {
+  baseUrl: "https://jsonplaceholder.typicode.com",
+};
+
+function checkRes(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject([`Ошибка ${res.status}`, res.json()]);
+}
+
+function request(url, options) {
+  return fetch(url, options)
+    .then(checkRes)
+}
+
+export async function getUsers() {
+  return await request(`${config.baseUrl}/users`, {
+    method: 'GET',
+  });
+}
+
+export async function getPosts() {
+  return await request(`${config.baseUrl}/posts`, {
+    method: 'GET',
+  });
+}
+
+export async function deletePostReq(id) {
+  return await request(`${config.baseUrl}/posts/${id}`, {
+    method: 'DELETE',
+  });
+}
